3d0d62dd73cd7c2fcd8f8443b529a3c1b44c6b74
SohanaN <sohananaz01@gmail.com>
2022-09-21 17:36:31 +0600

    heroku setup

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ app.use(cors());
 app.use(express.json());
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.czxb1sg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -18,6 +33,16 @@ async function run() {
     try {
         await client.connect();
         const inventoryCollection = client.db('eBook').collection('inventory');
+        const orderCollection = client.db('eBook').collection('order');
+
+        // AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
 
         // Inventory API
         app.get('/inventory', async (req, res) => {
@@ -48,6 +73,29 @@ async function run() {
             const result = await inventoryCollection.deleteOne(query);
             res.send(result);
         });
+
+        //order collection API
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+        });
+
+        // order 
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
 
     }
     finally {
